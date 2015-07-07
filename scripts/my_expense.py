c7687c4118067de93a95b3e00425b436244d2758
emily2 he <emily2.he@sonymobile.com>
2015-07-07 15:55:16 +0200

    Added TODO

--- scripts/my_expense.py
+++ scripts/my_expense.py
@@ -8,6 +8,10 @@ def print_list(expense_list):
     print "{0:10}\n{1:10} {2:2}".format(border, "Total", total)
     return total
 
+def montly_expense():
+    #Find the month and calculate weekly expense and do summary
+    print "Has not been implemented yet!"
+
 def weekly_expense():
     week = raw_input("Enter the week number ")
     week = week.lower()
@@ -34,5 +38,12 @@ def weekly_expense():
                 else:
                     print_list(expense)
                     break
-
-weekly_expense()
+option = raw_input("Weekly summary(1) or Monthly summary(2): ")
+try:
+    opt = int(option)
+except:
+    print "Please enter valid option!"
+if opt == 1:
+    weekly_expense()
+elif opt == 2:
+    monthly_expense()
